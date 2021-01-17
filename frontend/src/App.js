4b90a2ca0130eccb5a8b4b080eaedc0d313c331c
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-17 12:24:11 +0600

    React

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import Router from 'react'
+
+const App = () => {
+  return(
+    <>
+    <h1>WeleCome To ProShop</h1>
+    </>
+  )
+}
+
+
+
+// function App() {
+//   return (
+//     <>
+//     <h1>WeleCome To ProShop</h1>
+//     </>
+//   );
+// }
+
+export default App;
