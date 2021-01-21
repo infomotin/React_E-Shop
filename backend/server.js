fc5947c23af65b3ef7301f9f0dc8a71dce529833
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-21 16:37:11 +0600

    first commit 3rd single-page - install dependence script concurrently adding   install env pack error

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
-const express = require('express')
-    // adding 
-const dotenv = require('dotenv')
-const products = require('./data/products')
-    // env configerrations 
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 dotenv.config()
 
@@ -22,3 +20,11 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Sever is Running ${process.env.NODE_ENV} server  port is ${PORT}`))
+
+
+
+// const express = require('express')
+//     // adding 
+// const dotenv = require('dotenv')
+// const products = require('./data/products.js')
+//     // env configerrations
