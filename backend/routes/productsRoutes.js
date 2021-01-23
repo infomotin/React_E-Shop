bf02853b3602209fae47b2176baad782228acccc
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-23 15:21:12 +0600

    DB Setup model userSchame  productSchama product route compleat

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,13 +1,28 @@
 import express from 'express'
+import Product from '../model/productModel.js'
+import * as asyncHandler from 'express-async-handler'
 
 const router = express.Router()
-
-router.get('/api/products', (req, res) => {
+    //@desc Fetch all Product 
+    //@route GET api/products
+    //@access Public  
+router.get('/', asyncHandler((req, res) => {
+    const products = await Product.find({})
     res.json(products)
-})
-router.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+}))
+
+//@desc Fetch single Product 
+//@route GET api/product/:id 
+//@access Public
+
+router.get('/:id', asyncHandler((req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404).json({ message: 'Product Not in List ' })
+    }
+
+}))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import dotenv from 'dotenv'
 import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
-import routes from '../routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -27,6 +27,9 @@ app.get('/', (req, res) => {
     //     const product = products.find(p => p._id === req.params.id)
     //     res.json(product)
     // })
+
+app.use('/api/products', productRoutes)
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Sever is Running ${process.env.NODE_ENV} server  port is ${PORT}`.green.underline))
