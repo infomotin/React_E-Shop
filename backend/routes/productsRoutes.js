bdc03035bd3c19b008a1a63dcffdac98ed9a5bea
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-23 18:51:49 +0600

    DB Setup model userSchame  productSchama product route compleat fix

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -3,13 +3,17 @@ import Product from '../model/productModel.js'
 import asyncHandler from 'express-async-handler'
 
 const router = express.Router()
-
+    //@decs Fetch all product
+    //@route GET /api/products
+    //@access public
 router.get('/', asyncHandler(async(req, res) => {
     const products = await Product.find({})
     res.json(products)
 }))
 
-
+//@decs Fetch all product
+//@route GET /api/products
+//@access public
 router.get('/:id', asyncHandler(async(req, res) => {
     const product = await Product.findById(req.params.id)
     if (product) {
