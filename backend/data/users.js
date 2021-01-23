3c85035704d6d720d5e36377935ffa2269af3e36
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-23 11:39:20 +0600

    DB Setup model userSchame  productSchama seeder.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,14 +3,16 @@ import bcrypt from 'bcryptjs'
 const users = [{
     name: 'Admin User',
     email: 'admin@example.com',
-    password: bcrypt.hashSync('123456'),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: true
 }, {
     name: 'Jhon Dog',
     email: 'jhon@example.com',
-    password: xxxx
+    password: bcrypt.hashSync('123456', 10)
 }, {
     name: 'Motin Abdul',
     email: 'motin@example.com',
-    password: xxxx
+    password: bcrypt.hashSync('123456', 10)
 }]
+
+export default user
