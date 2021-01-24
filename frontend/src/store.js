fc0cbd08243f3930467a183da2d5e21cbf5083f9
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-24 11:52:58 +0600

    redux index js fix

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import reduxThunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({})
 const initialState = {}
 const middleware = [thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware([...middleware])))
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
 
 
 
