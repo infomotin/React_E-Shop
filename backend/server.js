9156588fd15b5326999c5ae90c05ce6fcd38a491
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-24 09:57:19 +0600

    DB Setup model userSchame  productSchama product error excep

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,14 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
     //Error Hendeler Define 
+    //lamda expertions
 app.use((err, req, res, next) => {
-
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    })
 })
 
 
