2edf3ea670f8173d5d3813bd99af6da248bd1277
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-24 11:09:19 +0600

    redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,3 +6,7 @@ const reducer = combineReducers({})
 const initialState = {}
 const middleware = [thunk]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware([...middleware])))
+
+
+
+export default store
