8a61b1441ebf3bad47788f68198790ba5d805425
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-24 10:16:00 +0600

    DB Setup model userSchame  productSchama product error inport Error Handeller

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productsRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -36,19 +37,11 @@ app.use('/api/products', productRoutes)
     //Error Hendeler Define 
     //lamda expertions
 
-app.use((req, res, next) => {
-    const error = new Error(`Not Found -${req.originalUrl}`)
-    res.status(404)
-    next(error)
-})
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-})
+app.use(notFound)
+
+
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
