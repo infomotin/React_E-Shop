0a331cba8055f6fbc695ffa0982199feeebe6264
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-25 15:47:33 +0600

    component adding single itemse add onclick cartReducers add cartlist

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import reduxThunk from 'redux-thunk';
+// import reduxThunk from 'redux-thunk';
 // import { composeWithDevTools } from 'redux-devtools-extension'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers, productDetailsReducers } from './reducers/productReducers'
+import { cartReducers } from './reducers/cartReducers'
 
 const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducers,
+    cartList: cartReducers,
 })
 const initialState = {}
 const middleware = [thunk]
