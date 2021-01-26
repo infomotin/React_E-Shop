8d7cd1bcd586331c3790918dd3303379c47fb0c8
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-26 10:07:04 +0600

    component adding single itemse add onclick cartReducers add cartAction

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,36 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstance'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM,CART_ERROR} from '../constants/cartConstance'
 import axios from 'axios'
 //getState come from store ....
-export const addToCart =(id,qty) =>async()
+//dstructure data {data}
+export const addToCart =(id,qty) =>async(dispatch,getState) =>{
+    try{
+        //if this actions success 
+        dispatch({type:CART_ADD_ITEM})
+        const {data} = await axios.get(`/api/products/${id}`)
+        dispatch({
+            payload:{
+                product:data._id,
+                name:data.name,
+                image:data.image,
+                price:data.price,
+                countInStock:data.countInStock
+            }
+        })
+
+    }
+    catch(error){
+        dispatch({
+            type: CART_ERROR,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message : error.message,
+        })
+    }
+
+
+
+
+
+
+    
+    
+}
