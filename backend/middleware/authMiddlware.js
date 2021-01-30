f81b92b83a78b8db8fcca0964b14d3fa3c8e8bab
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-30 16:58:19 +0600

    30 user authentic middleware

--- backend/middleware/authMiddlware.js
+++ backend/middleware/authMiddlware.js
@@ -1,12 +1,30 @@
 import jwt from 'jsonwebtoken'
+// import expressAsyncHandler from 'express-async-handler'
+
+import asyncHandler from 'express-async-handler'
 import User from '../model/userModel.js'
 
-const protect = async(req,res,next) =>{
-    let token = []
-    console.log(req.headers.authorization)
+const protect = asyncHandler( async(req,res,next) =>{
+    let token
+    
+    // const authorToken = req.headers.authorization
+    // console.log(authorToken)
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        console.log('Have Token')
 
+        try{
+            token = req.headers.authorization.split(' ')[1]
+            const decode = jwt.verify(token,process.env.JWT_TOKEN)
+        }catch{
+
+        }
+    }
+    if(!token){
+        res.status(401)
+        throw new Error('Not token')
+    }
     next()
 
-}
+})
 
 export {protect}
