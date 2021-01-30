2c495662f4afa80c52c826c2d5c8faa4f3666dde
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-30 10:36:25 +0600

    30 user authentic

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,25 @@
+import Product from '../model/productModel.js'
+import asyncHandler from 'express-async-handler'
+
+    //@decs Fetch all product
+    //@route GET /api/products
+    //@access public 
+const getProducts =asyncHandler(async(req, res) =>{
+    const products = await Product.find({})
+        // res.status(401)
+        // throw new Error('Not Error Just Test')
+    res.json(products)
+})
+//@decs Fetch all product
+//@route GET /api/products
+//@access public
+const getProductById =asyncHandler(async(req, res) =>{
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+            // .json({ message: 'Product Not in List ' })
+        throw new Error('Product Not in List')
+    }
+})
