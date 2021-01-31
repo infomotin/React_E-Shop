24a16a003f8d63825bfa2fde62ad5ec04d1f02b9
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-31 11:44:45 +0600

    30 user login

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,11 +5,13 @@ import reduxThunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers, productDetailsReducers } from './reducers/productReducers'
 import { cartReducers } from './reducers/cartReducers'
+import { userLoginReducers } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducers,
     cartList: cartReducers,
+    userLogin:userLoginReducers,
 })
 
 // //if cart items are store in local variable in can retripe from local stoare else set to inital default 
@@ -18,10 +20,12 @@ const reducer = combineReducers({
 //     JSON.stringify(getState().cartList.cartItems)
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 
 const initialState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userLogin: userInfoFromStorage },
 }
 const middleware = [reduxThunk]
 const store = createStore(
