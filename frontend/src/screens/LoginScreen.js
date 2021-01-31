e55938fb89415bb68f52068c227cece3ae873aa5
Abdul Motin <motin.mmk.jr@gmail.com>
2021-01-31 17:59:35 +0600

    30 user login  form ok

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -9,39 +9,48 @@ import {login} from '../actions/userActions'
 
 
 
-const LoginScreen = ({location}) => {
+const LoginScreen = ({location,history}) => {
 
 
     
-    const {email,setEmail} = useState('')
-    const {password,setPassword} = useState('')
+    const {email,setEmail} = useState(' ')
+    const {password,setPassword} = useState(' ')
+
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state=>state.userLogin)
+    const {loading,userInfo,error} = userLogin 
+
     const redirect = location.search ? location.search.split('=')[1]:'/'
+
+    useEffect(()=>{
+        if(userInfo){
+            history.push(redirect)
+        }
+    },[history,userInfo,redirect])
 
     const submitHandelerClick = (e)=>{
         e.preventDefault()
-        // dispatch login
+        dispatch(login(email,password))
     }
     return (
         <FormConteainer>
             <h1>SignIn</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loading />}
         <Form onSubmit={submitHandelerClick}>
+
             <Form.Group controlId='email'>
                 <Form.Label>
                     Email Adress
                 </Form.Label>
-                <Form.Control type='email' placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)}>
-
-                </Form.Control>
+                <Form.Control type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
             </Form.Group>
             <Form.Group controlId='password'>
                 <Form.Label>
                     Password
                 </Form.Label>
-                <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e)=>setPassword(e.target.value)}>
-
-                </Form.Control>
+                <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
             </Form.Group>
-
             <Button type='submit' variant='primary'>Sign In </Button>
 
         </Form>
